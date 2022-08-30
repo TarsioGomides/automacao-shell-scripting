272270c5c521475e8f3a333d53e051a72cb070f1
Tarsio Gomides <tarsiogomideslima@hotmail.com>
2022-08-30 02:42:56 -0300

    criarpasta de conversaocaso nao exista, englobar funcionalidade dentro de uma funcao, passar erros de execucao para dentro de um arquivo usando descritores de arquivos e verificar se houve sucesso ou falha na conversao

--- script-conversao-jpg-to-png.sh
+++ script-conversao-jpg-to-png.sh
@@ -1,9 +1,29 @@
 #! /bin/bash
-export PATH=/usr/bin/convert:$PATH
+converte_imagens(){
+    export PATH=/usr/bin/convert:$PATH
 
-cd /../imagens-livros
+    cd ../imagens-livros
 
-for imagem in $@
-do
-    convert $FOLDERPATH/$imagem.jpg $FOLDERPATH/convertido/$imagem.png
-done
+    #Verifica se a pasta para colocar as imagens convertidas existe, se não existir ela é criada
+    if [ ! -d convertido ]
+    then
+        mkdir convertido
+    fi
+
+    #Pega todas as imagens com extensão jpg, depois remove a extensão e por último faz a conversão para png
+    for imagem in *.jpg
+    do
+        imagem_sem_extensao=$(ls $imagem | awk -F. '{ print $1 }')
+        convert $imagem_sem_extensao.jpg convertido/$imagem_sem_extensao.png
+    done
+}
+
+converte_imagens 2>erros.txt
+
+#Para saber se a conversão ocorreu com sucesso ou se houveram falhas
+if [ $? -eq 0 ]
+then
+    echo "Conversão realizada com sucesso"
+else
+    echo "Houve uma falha no processo de conversão"
+fi
